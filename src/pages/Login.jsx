b6a97405ac6e574434f33252285d093c4fa8aadf
lags2022 <lguzman.58erb@outlook.com>
2023-04-04 18:09:38 -0500

    validations yup

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -2,6 +2,7 @@ import { View, Button, StyleSheet } from "react-native";
 import { Formik, useField } from "formik";
 import StyledTextInput from "../components/StyledTextInput";
 import StyledText from "../components/StyledText";
+import { loginValidationSchema } from "../validationSchemas/login";
 
 const initialValues = {
   email: "",
@@ -36,21 +37,22 @@ const FormikInputValue = ({ name, ...props }) => {
   );
 };
 
-const validate = (values) => {
-  const errors = {};
-  if (!values.email) {
-    errors.email = "Required";
-  } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
-    errors.email = "Invalid email address";
-  }
-  console.log(errors);
-  return errors;
-};
+// const validate = (values) => {
+//   const errors = {};
+//   if (!values.email) {
+//     errors.email = "Required";
+//   } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
+//     errors.email = "Invalid email address";
+//   }
+//   console.log(errors);
+//   return errors;
+// };
 
 export default function LogInPage() {
   return (
     <Formik
-      validate={validate}
+      validationSchema={loginValidationSchema}
+      // validate={validate}
       initialValues={initialValues}
       onSubmit={(values) => console.log(values)}
     >
