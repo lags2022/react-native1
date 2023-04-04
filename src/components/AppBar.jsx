617159c85ebaf27b53b9174a7dd77652381ef492
lags2022 <lguzman.58erb@outlook.com>
2023-04-04 11:01:49 -0500

    scroll

--- src/components/AppBar.jsx
+++ src/components/AppBar.jsx
@@ -1,26 +1,60 @@
-import { View, StyleSheet } from "react-native";
+import {
+  View,
+  StyleSheet,
+  TouchableWithoutFeedback,
+  ScrollView,
+} from "react-native";
 import StyledText from "./StyledText";
 import Constants from "expo-constants";
 import theme from "../theme";
+import { Link, useLocation } from "react-router-native";
 
 const styles = StyleSheet.create({
   container: {
     backgroundColor: theme.appBar.primary,
     paddingTop: Constants.statusBarHeight + 10,
-    paddingBottom: 10,
     paddingLeft: 10,
+    flexDirection: "row",
+  },
+  scroll: {
+    paddingBottom: 15,
   },
   text: {
+    color: theme.appBar.textSecondary,
+    paddingHorizontal: 10,
+  },
+  active: {
     color: theme.appBar.textPrimary,
   },
 });
 
+const AppBarTab = ({ children, to }) => {
+  const { pathname } = useLocation();
+  const active = pathname === to;
+
+  const textStyles = [styles.text, active && styles.active];
+
+  return (
+    <Link to={to} component={TouchableWithoutFeedback}>
+      <StyledText fontWeight="bold" style={textStyles}>
+        {children}
+      </StyledText>
+    </Link>
+  );
+};
+
 export default function AppBar() {
   return (
     <View style={styles.container}>
-      <StyledText fontWeight="bold" style={styles.text}>
-        Repositories
-      </StyledText>
+      {/* //puedes colocar active={true} o solo active */}
+      <ScrollView horizontal style={styles.scroll}>
+        <AppBarTab to="/">Repositories</AppBarTab>
+        <AppBarTab to="/signin">Sign In</AppBarTab>
+        <AppBarTab to="/register">Register</AppBarTab>
+        <AppBarTab to="/sub">Sub to Twich</AppBarTab>
+        <AppBarTab to="/midu">Midu hi!</AppBarTab>
+        <AppBarTab to="/about">About</AppBarTab>
+      </ScrollView>
     </View>
   );
 }
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,12 +1,17 @@
-import { View } from "react-native";
+import { View, Text } from "react-native";
 import RepositoryList from "./RepositoryList";
 import AppBar from "./AppBar";
+import { Routes, Route, Navigate } from "react-router-native";
 
 export default function Main() {
   return (
     <View style={{ flex: 1 }}>
       <AppBar />
-      <RepositoryList />
+      <Routes>
+        <Route path="/" exact element={<RepositoryList />} />
+        <Route path="/signin" element={<Text>Working on it</Text>} />
+        <Route path="*" element={<Navigate to="/" />} />
+      </Routes>
     </View>
   );
 }
